098059a61b1d162e667fb167e32928c82b478451
Andrii Nykolenko <nikolandre14@gmail.com>
2023-05-09 19:58:38 +0300

    create BaseNameClass with name and str method, create Profession and Param model inherited BaseNameClass

--- krok/models.py
+++ krok/models.py
@@ -1,3 +1,20 @@
+from django.core import validators
 from django.db import models
 
-# Create your models here.
+
+class BaseNameClass(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Profession(BaseNameClass):
+    pass
+
+
+class Param(BaseNameClass):
+    pass
